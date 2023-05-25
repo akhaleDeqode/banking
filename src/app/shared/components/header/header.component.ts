614a67ea9d1623f924020defc8b9c447487b1fea
akhaleDeqode <akhale@deqode.com>
2023-05-25 17:40:32 +0530

    Completed: navigation with sidebar

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subject, filter, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -8,6 +10,7 @@ import { Component, Input } from '@angular/core';
 export class HeaderComponent {
 
   @Input('userData') userData!: any;
+  breadCrumb!: any;
   optionList = [
     {
       label: 'Logout',
@@ -17,6 +20,43 @@ export class HeaderComponent {
       }
     }
   ];
+  private _unsubscribe$ = new Subject<boolean>();
 
+  constructor(
+    private _router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.updateBreadCrumb();
+    this._router.events.pipe(filter(event => event instanceof NavigationEnd), takeUntil(this._unsubscribe$)).subscribe((res: any) => {
+      this.updateBreadCrumb();
+    });
+  }
+
+  /**
+ * Function to update breadcrumb
+ */
+  updateBreadCrumb(): void {
+    const url = this._router.url;
+    switch (url) {
+      case '/dashboard/home':
+        this.breadCrumb = 'Dashboard';
+        break;
+      case '/withdrawls/list':
+        this.breadCrumb = 'Withdrawls';
+        break;
+      case '/deposits/list':
+        this.breadCrumb = 'Deposits';
+        break;
+      default:
+        this.breadCrumb = '';
+        break;
+    }
+  }
+
+  ngOnDestroy(): void {
+    this._unsubscribe$.next(true);
+    this._unsubscribe$.complete();
+  }
 
 }

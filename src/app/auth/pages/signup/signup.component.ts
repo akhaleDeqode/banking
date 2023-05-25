de79e910ec642eef3c3bd10abe96231fa7408887
akhaleDeqode <akhale@deqode.com>
2023-05-25 17:29:23 +0530

    Completed: signup form integration

--- src/app/auth/pages/signup/signup.component.ts
+++ src/app/auth/pages/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
+
+  signupForm!: FormGroup;
+  isFormSubmitted: boolean = false;
+
+  constructor(
+    private _formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.signupForm = this._formBuilder.group({
+      firstName: [null, [Validators.required]],
+      lastName: [null, [Validators.required]],
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]],
+      bankId: [null, Validators.required]
+    });
+  }
+
+  get FormControl() {
+    return this.signupForm.controls;
+  }
+
+  submit(): void {
+    this.isFormSubmitted = true;
+    console.log(this.signupForm.value);
+
+  }
 
 }

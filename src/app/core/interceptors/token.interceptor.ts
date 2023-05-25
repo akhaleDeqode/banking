4b21d15c44311edbb22adcd00ff31c819de02cda
akhaleDeqode <akhale@deqode.com>
2023-05-25 20:14:10 +0530

    Some changes done

--- src/app/core/interceptors/token.interceptor.ts
+++ src/app/core/interceptors/token.interceptor.ts
@@ -6,13 +6,31 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { LocalstorageService } from '../services/localstorage.service';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(
+    private _localStorageService: LocalstorageService
+  ) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const token = this._localStorageService.Token;
+    if (token) {
+      request = request.clone({
+        setHeaders: {
+          Bearer: token,
+          "Access-Control-Allow-Origin": "*"
+        }
+      });
+    } else {
+      request = request.clone({
+        setHeaders: {
+          "Access-Control-Allow-Origin": "*"
+        }
+      });
+    }
     return next.handle(request);
   }
 }
